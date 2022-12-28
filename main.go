9265e960134b37d67f02d8f0bae4cada8371ec89
Alberto Fabro <26629510+Eiliv17@users.noreply.github.com>
2022-12-28 12:44:15 +0100

    feat: add basic route to serve html file

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
+	r.LoadHTMLGlob("views/*")
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "main title",
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
